8b6385e82bb065c116f56a11396383f0f6e4760a
dev-rb <rbatra486@gmail.com>
2023-07-02 23:40:23 -0400

    Update null-ls source and format

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -10,7 +10,24 @@ local sources = {
 
   -- webdev stuff
   -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "tsx", "typescriptreact"} }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+    "css",
+    "scss",
+    "less",
+    "html",
+    "json",
+    "jsonc",
+    "yaml",
+    "markdown",
+    "markdown.mdx",
+    "graphql",
+    "handlebars",
+  }, -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
@@ -21,30 +38,30 @@ local sources = {
   require "typescript.extensions.null-ls.code-actions",
 }
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            -- apply whatever logic you want (in this example, we'll only use null-ls)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+  vim.lsp.buf.format {
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  }
 end
-
+
 -- if you want to set up formatting on save, you can use this as a callback
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
+
 -- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
+  end
 end
 null_ls.setup {
   debug = true,

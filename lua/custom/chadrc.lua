0f2977901deaabb3ca6ac7b4178bfaeb37498a09
dev-rb <rbatra486@gmail.com>
2023-12-05 12:43:57 -0500

    Disable swapfile. Skip ts commentstring.

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -23,4 +23,6 @@ vim.o.foldcolumn = "0" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.g.skip_ts_context_commentstring_module = true
+vim.opt.swapfile = false
 return M
